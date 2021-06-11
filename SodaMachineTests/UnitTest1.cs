f302dda027b54b939825a76b698e58fe83b0ac35
tjlambrecht <tjlambrecht@outlook.com>
2021-06-11 15:38:32 +0200

    Add xunit test project.

---
+++ SodaMachineTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace SodaMachineTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int i = 1;
+            Assert.True(i == 1);
+        }
+
+    }
+}
